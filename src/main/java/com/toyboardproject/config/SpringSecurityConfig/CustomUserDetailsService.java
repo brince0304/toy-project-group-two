d7bbb503d18e2aac8d18e2f55bc99bf8518d7e4a
brince0304 <brince0304@gmail.com>
2023-02-03 21:29:35 +0900

    [백석현] #VIEW - FEAT / 스프링 시큐리티 의존성 추가 및 기본 설정
    스프링 시큐리티를 위해서
     - 로그인시에 시큐리티 컨텍스트 홀더에 인증정보를 저장할 수 있도록 
     LoginSuccessHanlderCustom(AutheticationHandler 구현체) 클래스를 생성했습니다.
    - 로그아웃시에 인증정보와 세션을 만료시키기 위해 
     LogoutSuccessHandlerCustom 을 생성하였습니다.
    - 스프링 시큐리티 설정 클래스인 SecurityConfig 클래스를 생성하여 
     로그인시에 비밀번호 암호화, 로그인시에 리다이렉트 주소, 로그아웃시에 세션,쿠키 만료와 리다이렉트 주소 등을 설정하였습니다.
    
    - UserDetails 를 구현한 PrincipalDto 클래스를 생성했습니다.
    - UserDetailsService 를 구현한 CustomUserDetailsService 클래스를 생성했습니다.
     > 해당 클래스가 로그인시에 authentication 객체에 들어가는 principal 을 리턴하게 됩니다.

---
+++ src/main/java/com/toyboardproject/config/SpringSecurityConfig/CustomUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.toyboardproject.config.SpringSecurityConfig;
+
+import com.toyboardproject.Repository.AccountRepository;
+import com.toyboardproject.domain.Account;
+import com.toyboardproject.dto.PrincipalDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final AccountRepository accountRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Account account_ = accountRepository.findByUserId(username).orElseThrow(()->new UsernameNotFoundException("계정이 존재하지 않습니다."));
+
+        return PrincipalDto.builder()
+                .userId(account_.getUserId())
+                .userName(account_.getUserName())
+                .userPassword(account_.getUserPassword())
+                .userNickname(account_.getUserNickname())
+                .userPhone(account_.getUserPhoneNum()).build();
+
+    }
+}
