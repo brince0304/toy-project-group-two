e6b7b1ac6bb9bfb65f872849af13b7ba467c1fcc
Brince <110673427+brince0304@users.noreply.github.com>
2023-02-07 15:02:23 +0900

    [백석현] EDIT/ 게시글 -변경사항 정리

--- src/main/java/com/toyboardproject/controller/BoardController.java
+++ src/main/java/com/toyboardproject/controller/BoardController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Validated
+
 @RestController
 @RequestMapping("/board")
 @RequiredArgsConstructor
@@ -32,7 +32,7 @@ public class BoardController {
 
     //게시글 생성
     @PostMapping("/post")
-    public ResponseEntity<Long> createBoard(@Valid @RequestBody BoardDto dto, BoardType boardType, BindingResult bindingResult){
+    public ResponseEntity<Long> createBoard(@Valid @RequestBody BoardDto dto, BindingResult bindingResult){
         Long id = boardService.createBoard(dto, boardType);
 
         return new ResponseEntity<>(id, HttpStatus.OK);
@@ -40,74 +40,50 @@ public class BoardController {
 
     //게시글 생성 페이지 요청
     @GetMapping("/post")
-    public String openBoardWrite() {
-        return "board/post";
+    public ModelAndView getBoardInsertForm() {
+        ModelAndView mav = new ModelAndView();
+        //mav.setViewName("board/post");
+        return mav;
     }
 
     //게시글 상세 페이지
     @GetMapping("/{id}")
-    public ResponseEntity<BoardResponseDto> findBoardById(@PathVariable final Long id, Model model, BindingResult bindingResult) {
+    public ModelAndView findBoardById(@PathVariable Long id) {
         BoardResponseDto responseDto = boardService.findBoardById(id);
-        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        ModelAndView mav = new ModelAndView();
+        mav.addAttribute("boardResponse",responseDto);
+        //mav.setViewName("상세뷰")
+        return mav;
     }
 
     //게시글 리스트 페이지
     @GetMapping("/")
-    public ResponseEntity<List> boardList(BoardType type) {
-        List<BoardResponseDto> responseDtoList = boardService.findAll();
-        return new ResponseEntity<>(responseDtoList, HttpStatus.OK);
+    public ModelAndView boardList(@RequestParam BoardType type, SearchType searchType, String keyword) {
+        Page<BoardResponseDto> response = boardService.searchBoardByBoardTypeAndSearchTypeAndKeyWord(type,searchType,keyword);
+        ModelAndView mav = new ModelAndView();
+        //mav.setViewName("/boards/")
+        //mav.addAttribute("boardList",response);
+        //mav.addAttribute("boardType",type);
+        //mav.addAttribute("searchType",searchType);
+        //mav.addAttribute("keyword",keyword);
+        return mav;
     }
 
-    //게시글 리스트 조회(삭제된거 빼고 출력되어야 함)
-//    @GetMapping("/")
-//    public List<BoardResponseDto> findAll(@RequestParam final boolean isDeleted){
-//        return boardService.findAllWithoutDelete(true);
-//    }
+
 
 
     // 게시글 업데이트
     @PutMapping("/{id}")
-    public ResponseEntity<Long> updateBoardById(@PathVariable Long id, @RequestBody BoardDto dto, BoardType boardType, BindingResult bindingResult){
-        Long updateBoardId = boardService.updateBoardById(id, dto, boardType);
-        return new ResponseEntity<>(updateBoardId, HttpStatus.OK);
+    public ResponseEntity<Long> updateBoardById(@PathVariable Long id,@Valid @RequestBody BoardDto dto, BindingResult bindingResult){
+        return new ResponseEntity<>(boardService.updateBoardById(id, dto, boardType), HttpStatus.OK);
     }
 
     // 게시글 삭제
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> deleteBoardById(@PathVariable Long id, BoardType boardType, BindingResult bindingResult){
-        boolean result = boardService.deleteBoardById(id, boardType);
-        return new ResponseEntity<>(result, HttpStatus.OK);
+    public ResponseEntity<Boolean> deleteBoardById(@PathVariable Long id){
+        return new ResponseEntity<>(boardService.deleteBoardById(id, boardType), HttpStatus.OK);
     }
 
-    //페이징
-    @GetMapping("/")
-    public String paging(Model model, BoardType boardType,
-                         @PageableDefault(size = 20, sort = "id", direction = Sort.Direction.ASC)Pageable pageable){
 
-        Page<Board> list = boardService.paging(boardType, pageable);
 
-        model.addAttribute("paging",list);
-        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
-        model.addAttribute("next", pageable.next().getPageNumber());
-        model.addAttribute("hasNext", list.hasNext());
-        model.addAttribute("hasPrev", list.hasPrevious());
-        return "board/index";
-    }
-
-    // 검색 기능
-//    @GetMapping("/board/search")
-//    public String search(String keyword, Model model,
-//                         @PageableDefault(sort = "id", direction = Sort.Direction.ASC)
-//                         Pageable pageable) {
-//        Page<Board> searchList = boardService.search(keyword, pageable);
-//
-//        model.addAttribute("searchList", searchList);
-//        model.addAttribute("keyword", keyword);
-//        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
-//        model.addAttribute("next", pageable.next().getPageNumber());
-//        model.addAttribute("hasNext", searchList.hasNext());
-//        model.addAttribute("hasPrev", searchList.hasPrevious());
-//
-//        return "posts-search";
-//    }
 }
