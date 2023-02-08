ae70fd25096d00ae259a7a80fb31837ae0e96629
dont-octopus <jubsuny@gmail.com>
2023-02-09 01:26:06 +0900

    [조윤영] Board FEAT/ BoardController 내용 수정 - 게시글 생성, 수정 ,삭제 메서드에 @AuthCheck 추가 - updateBoardById 메서드의 @PutMapping의 uri 수정 & 매개변수와 return에 principal 추가 - deleteBoardById 메서드와 구현부에 principal 추가 - 게시글 생성 및 수정 요청 페이지 합쳐서 구현 - [백석현] findBoardById 메서드의 ModelAndView viewName 추가

--- src/main/java/com/toyboardproject/controller/BoardController.java
+++ src/main/java/com/toyboardproject/controller/BoardController.java
@@ -3,13 +3,17 @@ package com.toyboardproject.controller;
 import com.toyboardproject.Annotation.AuthCheck;
 import com.toyboardproject.Annotation.BindingCheck;
 import com.toyboardproject.domain.BoardType;
+import com.toyboardproject.domain.SearchType;
 import com.toyboardproject.dto.BoardRequestDto;
 import com.toyboardproject.dto.BoardResponseDto;
 import com.toyboardproject.dto.PrincipalDto;
 import com.toyboardproject.service.BoardService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,16 +22,19 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.net.URI;
 
-@RestController
+
 @RequestMapping("/board")
 @RequiredArgsConstructor
+@RestController
 public class BoardController {
 
     private final BoardService boardService;
 
 
     //게시글 생성
+    @AuthCheck
     @PostMapping("/post")
     @BindingCheck
     public ResponseEntity<Long> createBoard(@AuthenticationPrincipal PrincipalDto principal, @Valid @RequestBody BoardRequestDto dto, BindingResult bindingResult){
@@ -35,12 +42,18 @@ public class BoardController {
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
-    //게시글 생성 페이지 요청
+    // 게시글 생성 및 수정 페이지 요청
     @GetMapping("/post")
     @AuthCheck
-    public ModelAndView getBoardInsertForm(@AuthenticationPrincipal PrincipalDto principal) {
+    public ModelAndView getBoardInsertForm(@RequestParam BoardType type,@RequestParam(required = false) Long id,@AuthenticationPrincipal PrincipalDto principal) {
         ModelAndView mav = new ModelAndView();
+        BoardResponseDto responseDtoId = boardService.findBoardById(id);
+        mav.addObject("boardType", type);
         mav.setViewName("board/post");
+
+        if(id > 0){
+            mav.addObject("responseDtoId", responseDtoId);
+        }
         return mav;
     }
 
@@ -55,31 +68,31 @@ public class BoardController {
     }
 
     //게시글 리스트 페이지
-    //@GetMapping("/")
-    //public ModelAndView boardList(@RequestParam BoardType type, SearchType searchType, String keyword) {
-        //Page<BoardResponseDto> response = boardService.searchBoardByBoardTypeAndSearchTypeAndKeyWord(type,searchType,keyword);
-        //ModelAndView mav = new ModelAndView();
-        //mav.setViewName("/board/index")
-        //mav.addAttribute("boardList",response);
-        //mav.addAttribute("boardType",type);
-        //mav.addAttribute("searchType",searchType);
-        //mav.addAttribute("keyword",keyword);
-        //return mav;
-    //}
-
-
+    @GetMapping("/")
+    public ModelAndView boardList(@RequestParam BoardType type, @RequestParam SearchType searchType, @RequestParam String keyword, Pageable pageable) {
+        Page<BoardResponseDto> response = boardService.searchBoardByKeywordAndSearchTypeAndBoardType(pageable,type,searchType,keyword);
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/board/index");
+        mav.addObject("boardList",response);
+        mav.addObject("boardType",type);
+        mav.addObject("searchType",searchType);
+        mav.addObject("keyword",keyword);
+        return mav;
+    }
 
 
     // 게시글 업데이트
-    @PutMapping("/{id}")
-    public ResponseEntity<Long> updateBoardById(@PathVariable Long id, @Valid @RequestBody BoardRequestDto dto, BindingResult bindingResult){
-        return new ResponseEntity<>(boardService.updateBoardById(id, dto), HttpStatus.OK);
+    @AuthCheck
+    @PutMapping("/")
+    public ResponseEntity<Long> updateBoardById(@RequestParam Long id, @Valid @RequestBody BoardRequestDto dto, @AuthenticationPrincipal PrincipalDto principal, BindingResult bindingResult){
+        return new ResponseEntity<>(boardService.updateBoardById(id, dto, principal.getId()), HttpStatus.OK);
     }
 
     // 게시글 삭제
+    @AuthCheck
     @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> deleteBoardById(@PathVariable Long id){
-        boardService.deleteBoardById(id);
+    public ResponseEntity<Boolean> deleteBoardById(@PathVariable Long id, @AuthenticationPrincipal PrincipalDto principal){
+        boardService.deleteBoardById(id, principal.getId());
         return new ResponseEntity<>( HttpStatus.OK);
     }
 
