03710914de06e9099fcfe9269763865172edd7bd
dont-octopus <jubsuny@gmail.com>
2023-02-09 04:06:44 +0900

    [조윤영] Board FEAT/ BoardRepository 메서드 추가

--- src/main/java/com/toyboardproject/repository/BoardRepository.java
+++ src/main/java/com/toyboardproject/repository/BoardRepository.java
@@ -2,10 +2,19 @@ package com.toyboardproject.repository;
 
 import com.toyboardproject.domain.Board;
 
+import com.toyboardproject.domain.BoardType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    Page<Board> findByBoardTitleContaining(BoardType boardType, String keyword, Pageable pageable);
+    Page<Board> findByBoardContentContaining(BoardType boardType, String keyword, Pageable pageable);
+    Page<Board> findByAccount_UserIdContaining(BoardType boardType, String keyword, Pageable pageable);
+    Page<Board> findByAccount_UserNicknameContaining(BoardType boardType, String keyword, Pageable pageable);
+
+
 
 }
-
