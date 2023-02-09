1d21f688cb92f7d00e3a86cd8ac512514d8bff93
brince0304 <brince0304@gmail.com>
2023-02-09 15:37:28 +0900

    [백석현] #Board - FEAT / 게시판 변경사항
    -공지사항과 FAQ에 대한 작성폼은 일반 유저가 접근할 수 없도록 변경하였습니다.
    -코드를 살짝 줄여주었습니다.
    -리스트 페이지에서 FAQ를 요청하면 리다이렉트 하도록 수정하였습니다.
    -FAQ뷰를 추가하였습니다.

--- src/main/java/com/toyboardproject/controller/BoardController.java
+++ src/main/java/com/toyboardproject/controller/BoardController.java
@@ -8,16 +8,21 @@ import com.toyboardproject.dto.BoardRequestDto;
 import com.toyboardproject.dto.BoardResponseDto;
 import com.toyboardproject.dto.PrincipalDto;
 import com.toyboardproject.service.BoardService;
+import com.toyboardproject.utils.BoardUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -29,9 +34,7 @@ import java.net.URI;
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
-
     private final BoardService boardService;
-
 
     //게시글 생성
     @AuthCheck
@@ -45,57 +48,85 @@ public class BoardController {
     // 게시글 생성 및 수정 페이지 요청
     @GetMapping("/post")
     @AuthCheck
-    public ModelAndView getBoardInsertForm(@RequestParam(required = false) BoardType type,@RequestParam(required = false) Long id,@AuthenticationPrincipal PrincipalDto principal) {
-        ModelAndView mav = new ModelAndView();
-        BoardResponseDto responseDtoId = boardService.findBoardById(id);
-        mav.addObject("boardType", type);
-        mav.setViewName("board/post");
-
-        if(id > 0){
-            mav.addObject("responseDtoId", responseDtoId);
+    public ModelAndView getBoardInsertForm(@RequestParam BoardType type,@RequestParam(required = false) Long id,@AuthenticationPrincipal PrincipalDto principal) {
+        if(type!=BoardType.FREE){
+            if(!principal.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
+                return new ModelAndView("redirect:/board/?type="+type);
+            }
+            if(!principal.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"))){
+                if(type.getValue().contains("FAQ")){
+                    return new ModelAndView("redirect:/board/faq/"+type);
+                }
+            }
         }
-        return mav;
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("board/post");
+        mav.addObject("boardType", type);
+        if(id==null) {
+            return mav;
+        }
+            mav.addObject("boardResponse", boardService.findBoardById(id));
+            return mav;
     }
 
     //게시글 상세 페이지
     @GetMapping("/{id}")
     public ModelAndView findBoardById(@PathVariable Long id) {
-        BoardResponseDto responseDto = boardService.findBoardById(id);
         ModelAndView mav = new ModelAndView("board/detail");
-        mav.addObject("boardResponse",responseDto);
+        mav.addObject("boardResponse",boardService.findBoardById(id));
         //mav.setViewName("상세뷰")
         return mav;
     }
 
     //게시글 리스트 페이지
     @GetMapping("/")
-    public ModelAndView boardList(@RequestParam(required = false) BoardType type, @RequestParam(required = false) SearchType searchType, @RequestParam(required = false) String keyword, Pageable pageable) {
+    public ModelAndView boardList(@RequestParam BoardType type, @RequestParam(required = false) SearchType searchType
+            , @RequestParam(required = false) String keyword
+            ,@PageableDefault(sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable) {
+        if(type.getValue().contains("FAQ")){
+            return new ModelAndView("redirect:/board/faq/FAQ_PAYMENT");
+        }
         Page<BoardResponseDto> response = boardService.searchBoardByKeywordAndSearchTypeAndBoardType(pageable,type,searchType,keyword);
+        if(searchType==null){
+            searchType=SearchType.TITLE;
+        }
+        if(keyword==null){
+            keyword="";
+        }
         ModelAndView mav = new ModelAndView();
         mav.setViewName("/board/index");
         mav.addObject("boardList",response);
-        mav.addObject("boardType",type);
+        mav.addObject("boardType", BoardUtil.getBoardTypeName(type));
         mav.addObject("searchType",searchType);
         mav.addObject("keyword",keyword);
+        return mav;
+    }
+
+    @GetMapping("/faq/{type}")
+    public ModelAndView faqList(@PathVariable BoardType type
+            ,@PageableDefault(sort="createdAt",direction = Sort.Direction.DESC) Pageable pageable) {
+        Page<BoardResponseDto> response = boardService.searchBoardByKeywordAndSearchTypeAndBoardType(pageable,type,null,"");
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("/board/faq");
+        mav.addObject("boardList",response);
+        mav.addObject("boardType", BoardUtil.getBoardTypeName(type));
         return mav;
     }
 
 
     // 게시글 업데이트
     @AuthCheck
-    @PutMapping("/")
-    public ResponseEntity<Long> updateBoardById(@RequestParam Long id, @Valid @RequestBody BoardRequestDto dto, @AuthenticationPrincipal PrincipalDto principal, BindingResult bindingResult){
-        return new ResponseEntity<>(boardService.updateBoardById(id, dto, principal.getId()), HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<Long> updateBoardById(@PathVariable Long id, @Valid @RequestBody BoardRequestDto dto, @AuthenticationPrincipal PrincipalDto principal, BindingResult bindingResult){
+        return new ResponseEntity<>(boardService.updateBoardById(id, dto), HttpStatus.OK);
     }
 
     // 게시글 삭제
     @AuthCheck
     @DeleteMapping("/{id}")
     public ResponseEntity<Boolean> deleteBoardById(@PathVariable Long id, @AuthenticationPrincipal PrincipalDto principal){
-        boardService.deleteBoardById(id, principal.getId());
+        boardService.deleteBoardById(id);
         return new ResponseEntity<>( HttpStatus.OK);
     }
-
-
 
 }
